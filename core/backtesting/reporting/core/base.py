4d28ed8c8c745fbbac681d1b06e9a580697f86d2
filip <filip1folga@gmail.com>
2026-01-28 18:58:54 +0100

    feat(reporting): add base abstract contracts for metrics, aggregators and reports

---
+++ core/backtesting/reporting/core/base.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class BaseMetric(ABC):
+    name: str
+
+    @abstractmethod
+    def compute(self, df: pd.DataFrame):
+        pass
+
+
+class BaseAggregator(ABC):
+    @abstractmethod
+    def aggregate(self, df: pd.DataFrame, metrics: list[BaseMetric]):
+        pass
+
+
+class BaseReport(ABC):
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    @abstractmethod
+    def run(self):
+        pass
